2c1763fac9d3a4d539d9e9e6b5920e720e570a5b
joaovictordg <84694176+joaovictordg@users.noreply.github.com>
2022-06-12 16:09:25 -0300

    Create MinhaBiblioteca.h

---
+++ MinhaBiblioteca.h
@@ -0,0 +1,6 @@
+#ifndef MINHABIBLIOTECA_H
+#define MINHABIBLIOTECA_H
+
+int retornaQuatidadeDeTrocas(char * mensagemAntiga, int tamanhoMensagem, char antigo, char novo, char * novaMensagem);
+
+#endif //MINHABIBLIOTECA_H
